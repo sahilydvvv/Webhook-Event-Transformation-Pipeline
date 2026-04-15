5c786b1af4b65d60d2040aa7eb04774c5b176ba7
sahil <sahhillyddvv@gmail.com>
2026-04-15 19:17:23 +0530

    fix

--- frontend/src/services/ruleService.js
+++ frontend/src/services/ruleService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/rules',
+  baseURL: 'http://localhost:5000/api/rules',
   withCredentials: true,
 });
 
